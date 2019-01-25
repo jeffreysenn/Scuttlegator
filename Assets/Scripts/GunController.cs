ba5c709176dfe5d3c42d238ad0770b5e004d87ba
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2019-01-25 23:12:43 +0100

    Aiming system

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null) { return; }
+        AimAtMouse();
+
+    }
+
+    void AimAtMouse()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, transform.position.z));
+        transform.right = (Vector2)(mouseWorld - player.transform.position);
+    }
+}
