4048f729e0b2064c8b472ef84643d82310217e85
vororo <adamolsson@outlook.com>
2019-01-26 10:47:29 +0100

    Made a main menu and a start button

---
+++ Assets/RopeBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Object stops at the rigidbody it collides with, render a linerenderer between it and the players hand (?).
+          The next shot from the player connects the second point for the linerenderer and spring joint*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
