072a827d7d13bf05b80d708a49bbe8f9c1855a6a
Marcus Ford <mackanford@hotmail.com>
2019-01-26 12:41:26 +0100

    Successful Spawn

---
+++ Assets/Scripts/BoulderSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderSpawn : MonoBehaviour
+{
+    public GameObject boulder;
+    private bool _hasSpawned = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && _hasSpawned == false)
+        {
+            SpawnBoulder();
+        }
+    }
+
+    private void SpawnBoulder()
+    {
+        Instantiate(boulder, transform.position, Quaternion.identity);
+        _hasSpawned = true;
+        Destroy(gameObject);
+    }
+}
