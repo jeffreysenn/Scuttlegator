ed97cde979e3ee43bf343e4df733bbc452c05125
vororo <adamolsson@outlook.com>
2019-01-26 16:02:28 +0100

    Made a health for the house  and a lose and win scene. Also did a goal.

---
+++ Assets/Scripts/HouseHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HouseHealth : MonoBehaviour
+{
+
+    private float health;
+    private float damage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        damage = 25;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            if ((health -= damage) <= 0)
+            {
+                SceneManager.LoadScene("LoseScene");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
