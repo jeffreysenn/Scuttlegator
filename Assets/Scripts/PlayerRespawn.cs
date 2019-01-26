e362d2cdf814c724b1ac83d2f60f7df05e89eda0
clep <clepirelli@gmail.com>
2019-01-26 20:37:38 +0100

    respawn

---
+++ Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+
+    public float spawnOffset = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            Vector3 spawnPos = GameObject.FindGameObjectWithTag("House").transform.position;
+            spawnPos.y += spawnOffset;
+            transform.position = spawnPos;
+        }
+    }
+}
