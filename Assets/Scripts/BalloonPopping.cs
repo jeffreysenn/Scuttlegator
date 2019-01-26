627e2f95b51b3b6709bb43498f7d360762a74050
clep <clepirelli@gmail.com>
2019-01-26 17:30:21 +0100

    new house

--- Assets/Scripts/BalloonPopping.cs
+++ Assets/Scripts/BalloonPopping.cs
@@ -11,4 +11,12 @@ public class BalloonPopping : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Balloon"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
