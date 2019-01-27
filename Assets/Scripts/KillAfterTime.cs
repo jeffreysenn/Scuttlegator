2bc9b9bb3f82a2a28c5b21675f08b1477aa2df28
clep <clepirelli@gmail.com>
2019-01-27 10:34:15 +0100

    balloons pop!

---
+++ Assets/Scripts/KillAfterTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAfterTime : MonoBehaviour
+{
+    public float LifeTimeInSeconds;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Death", LifeTimeInSeconds);
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+}
