cbcfa03c9834a6c7af2fa069afb51166d0e446e6
clep <clepirelli@gmail.com>
2019-01-27 15:47:40 +0100

    main menu yay!

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,20 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    public GameObject startButton;
+    public GameObject controlsButton;
+    public GameObject controlsSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.SetActive(false);
+        controlsSprite.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void showControls()
     {
-        
+        startButton.SetActive(true);
+        controlsSprite.SetActive(true);
+        controlsButton.SetActive(false);
     }
 
     public void startGame()
     {
-        SceneManager.LoadScene (1); 
+        SceneManager.LoadScene(1);
     }
 }
