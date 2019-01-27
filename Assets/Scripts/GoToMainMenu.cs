59e927c08739c0f882d0a230e656f317823eddd2
clep <clepirelli@gmail.com>
2019-01-27 16:01:00 +0100

    go back to main menu after winscreen

---
+++ Assets/Scripts/GoToMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMainMenu : MonoBehaviour
+{
+    public float timer = 10.0f;
+    private void Start()
+    {
+        Invoke("DoItPls", timer);
+    }
+
+    public void DoItPls()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
